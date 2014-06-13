53df436da9f9498526b493eb4fd50796348a8065
Peege151 <Pjsulli2k@gmail.com>
2014-06-12 20:28:53 -0400

    add gitignore also stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
+  :publishable_key => pk_test_bqdvwmEFZzufQOI01taGdkDr,
+  :secret_key      => sk_test_Dqq9iDrvWt4Yf7YQjKmmPvzs
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
