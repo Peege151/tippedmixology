6b231c1b77978e8c8b1aa91a4d1ee84a53fa1ef5
Peege151 <Pjsulli2k@gmail.com>
2014-06-25 01:08:53 -0400

    remove raise from mailer (sigh)

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -7,7 +7,6 @@ class OrderMailer < ActionMailer::Base
   end
 
   def confirmation(order)
-  		raise
   		@order = order
   		mail(to: @order.email, subject: "Order Confirmation", content_type: "text/html")
   		attachments.inline['tippedlogods.png'] = File.read("app/assets/images/tippedlogods.png")
